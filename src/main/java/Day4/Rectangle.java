8a275c7fbb6b48b3ae6cc4e8f9c60d4928f38946
mostafaali923 <mostafaali923.ma@gmail.com>
2026-06-07 18:41:11 +0300

    commit ad

--- src/main/java/Day4/Rectangle.java
+++ src/main/java/Day4/Rectangle.java
@@ -2,9 +2,56 @@ package Day4;
 
 // class =>> blueprint , container
 
+import java.util.Scanner;
+
+// Encapsulation => data hiding , data protection , data secuirty
+
 public class Rectangle {
 
-    public double length;
-    public double width;
+    // Data hiding: Prevent direct modification of field
+    // Benefit: improves security, maintainabilty and flexabilty
+    Scanner input = new Scanner(System.in);
 
-}
+
+    private double length; //field
+    private double width;
+
+    public void setLength(double length)
+    {
+        if (length > 0)
+            this.length = length;
+        else
+            System.out.println("Length must be positive.");
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setWidth(double width)
+    {
+        if (width > 0)
+            this.width = width;
+        else
+            System.out.println("Width must be positive.");
+    }
+     public double getWidth() {
+        return width;
+    }
+ public double calculateArea() { // method
+     return length * width;
+
+    }
+
+
+//    public double calculateArea() { // method
+//        System.out.println("Enter Length of rectangle");
+//        length = input.nextDouble();
+//        System.out.println("Enter Width of rectangle");
+//        width = input.nextDouble();
+//        double area = length * width;
+//        System.out.println("The area of the rectangle is: " + area);
+//        return area;
+    }
+
+
--- src/main/java/Day4/main.java
+++ src/main/java/Day4/main.java
@@ -1,26 +1,56 @@
 package Day4;
 
+import javax.sound.midi.Soundbank;
+
 public class main {
 
     void main(String[] args) {
 
-        Sum s = new Sum();
-        s.sum2Number(10, 20);
+//        Sum s = new Sum();
+//        s.sum2Number(10, 20);
+//
+//        s.sum2Number(10, 40);
+//
+//        hamada();
+//        total(10, 20);
 
-        s.sum2Number(10, 40);
+//        Rectangle rectangle = new Rectangle();
+//        double x = rectangle.calculateArea();
+//
+//        System.out.println("the valu of x is " +x);
 
-        hamada();
-        total(10, 20);
+        Rectangle r1 = new Rectangle();
+        r1.setLength(5.5);
+        r1.setWidth(3.5);
+        double area0fRoom1 = r1.calculateArea();
+        System.out.println("The area of room 1 is: " + area0fRoom1);
+
+        Rectangle r2 = new Rectangle();
+        r2.setLength(10.5);
+        r2.setWidth(10.5););
+        double area0fRoom2 = r2.calculateArea();
+        System.out.println("The area of room 2 is: " + area0fRoom2);
+
+        System.out.println("------------------------------------");
+        double totalArea = area0fRoom1 + area0fRoom2;
+        System.out.println("The total area of the two rooms is: " + totalArea);
     }
 
-    public static void hamada()
-    {
-        System.out.println("Hello, Hamada");
-    }
 
-    public static void total(int x, int y)
-    {
-        int sum = x + y;
-        System.out.println("The total is: " + sum);
-    }
+
+
+
+
+
+//
+//    public static void hamada() {
+//        System.out.println("Hello, Hamada");
+//    }
+//
+//    public static void total(int x, int y) {
+//        int sum = x + y;
+//        System.out.println("The total is: " + sum);
+//
+//
+//    }
 }
