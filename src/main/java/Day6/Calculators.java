0d4968362e9eb81c829b780f6833390601a31bce
mostafaali923 <mostafaali923.ma@gmail.com>
2026-06-14 02:58:34 +0300

    commit update 25

---
+++ src/main/java/Day6/Calculators.java
@@ -0,0 +1,10 @@
+package Day6;
+
+public interface Calculators {
+
+    int add(int a, int b);
+
+    double add(double a, double b);
+
+    int add(int a, int b, int c);
+}
--- src/main/java/Day6/Shape.java
+++ src/main/java/Day6/Shape.java
@@ -1,14 +1,32 @@
 package Day6;
 
-public class Shape {
+public abstract class Shape {
 
     // Abstraction: hiding implementation details and showing only the essential behavior
 
     // Focus on = > what an object does, not how it does it,
 
     /*
-    In real life: When you drive a car, you use the steering wneel and pedals (interface), but you don't worry about how the engine works (implementation details). You just know that when you turn the wheel or press the pedal, the car responds accordingly. This is abstraction in action.
+    In real life: When you drive a car, you use the steering wheel and pedals (interface),
+     but you don't worry about how the engine works (implementation details).
      */
 
+/*
+1- Abstract classes
+- Can nave both abstract methods (nobody) and concrete methods.
+• Cannot be instantiated directly
+
+2- Interfaces
+- Pure abstraction (before java  8: only abstract methods; Later: default and static methods allowed)
+- class can implement multiple interface
+
+ */
+
+    abstract void draw(); // abstract method: nobody, only signature
+
+    void info() // concrete method
+    {
+        System.out.println("Shape info: This is a shape.");
+    }
 
 }
