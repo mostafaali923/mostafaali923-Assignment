c81b8847bfab4b1c5dd98a846f9c7e2da97eccdb
mostafaali923 <mostafaali923.ma@gmail.com>
2026-06-21 13:46:57 +0300

    شي

---
+++ src/main/java/Day8/Colllection.java
@@ -0,0 +1,174 @@
+package Day8;
+
+import java.util.*;
+
+
+public class Colllection {
+    public static void main(String[] args) {
+        // Arrays are fixed size - once created can't resize them
+        // Collections are dynamic, more powerful, built in methodS
+
+        String[] names = new String[3];
+
+        names[0] = "Aya";
+        names[1] = "Yassin";
+        names[2] = "Ahmed";
+        //  names [3]= "Islam"; out of index
+
+        //collections solve this problem add, remove, search, sort
+
+        /*
+
+                            Collection (Interface)
+                             |
+      -------------------------------------------------
+      |                       |                       |
+   List                  Set                    Queue
+      |                       |                       |
+  ----------          ----------------       ----------------
+  |    |    |          |      |      |       |              |
+ArrayList LinkedList Vector HashSet LinkedHashSet TreeSet LinkedList PriorityQueue
+
+
+                    Map (Interface)
+                           |
+      ------------------------------------------------
+      |                  |               |           |
+   HashMap         LinkedHashMap      TreeMap    Hashtable
+
+         */
+
+
+ /*
+     Collection → Base interface for List, Set, and Queue.
+     List → Ordered collection, allows duplicates.
+     Set → No duplicate elements, no index.
+     Queue → FIFO (First In, First Out) order. Examples: LinkedList, PriorityQueue.
+     Map → Stores data as key-value pairs. Example: HashMap
+  */
+
+//        arraylistDemo();
+//        hashSetDemo();
+//        ListDemo();
+
+//        vectorDemo();
+
+
+        queue();
+
+    }
+
+//ArrayList
+
+    static void arraylistDemo() {
+        ArrayList<String> list = new ArrayList<>(); // No size limit
+        list.add("Ahmed");
+        list.add("Ahmed");
+        list.add("Marwa");
+        list.add("Sasa");
+        list.add("Doha");
+
+        System.out.println(list);
+        System.out.println(list.get(2));//Marwa
+        System.out.println(list.size());//5
+        System.out.println(list.getFirst());//ahmed
+        System.out.println(list.getLast()); // doha
+
+        list.remove("Ahmed"); //removes first occurrence of "ahmed"
+        list.remove(3);
+        System.out.println(list);
+
+        for (String s : list)
+            System.out.println(s);
+
+        //Lambda Expression : A Lambda Expression in Java is a short way to implement a functional interface
+        // (an interface with one abstract method) without creating a class, using the -> syntax.
+        list.forEach(System.out::println);
+        list.forEach(s -> System.out.println(s));
+
+        System.out.println(list.contains("Marwa")); //true
+        System.out.println(list.contains("Doha")); //false
+        /*
+        Dynamic sizing
+        methods add(), get(), remove(), size(), and contains()
+        Enhanced for-loop works seamlessly
+         */
+    }
+
+    // HashSet (No duplicated )
+
+
+    static void hashSetDemo() {
+
+        HashSet<String> colors = new HashSet<>();
+        // Set<String> colors = new HashSet<>(); طريقه اخري
+
+        colors.add("Red");
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+
+
+        System.out.println(colors); // [Red, Blue, Green] - no Duplicate
+    }
+
+    // List => Array List , Linked list , Vector , Stack
+
+    static void ListDemo() {
+
+        LinkedList<Object> items = new LinkedList<>();
+        items.add("Apple");
+        items.add(2);
+        items.add(2.6);
+        items.add(true);
+
+        System.out.println(items);
+        System.out.println(items.get(2));
+        System.out.println(items.size());
+        System.out.println(items.getFirst());
+        System.out.println(items.getLast());
+
+    }
+
+    static void vectorDemo() {
+        Vector<String> v = new Vector<>();
+
+        v.add("A");
+        v.add("B");
+        v.add("C");
+
+        System.out.println(v);
+    }
+
+    static void stackDemo() {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println(stack);
+        System.out.println(stack.pop());
+
+    }
+    // Linked list - Priorty Queue
+
+    static void queue() {
+
+        Queue<Object> q = new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add("ROUTE");
+        q.add(4);
+        q.add("JAVA");
+        q.add("JAVA");
+
+        System.out.println(q);
+        System.out.println(q.size());
+        System.out.println(q.remove()); // يحذف اول عنصر 1
+
+    }
+
+}
